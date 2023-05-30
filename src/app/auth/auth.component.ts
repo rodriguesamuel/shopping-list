e8c02df0547d30d8f6a48be63cc5a0a113344498
samuel <samuel.bruno.aguiar@gmail.com>
2023-05-30 19:11:41 -0300

    loading, catch error

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -8,7 +8,8 @@ import {AuthService} from "./auth.service";
 })
 export class AuthComponent {
   isLoginMode = true;
-
+  isLoading = false;
+  error: string = null;
 
   constructor(private authService: AuthService) {
   }
@@ -24,14 +25,19 @@ export class AuthComponent {
     const email = authForm.value.email;
     const password = authForm.value.password;
 
+    this.isLoading = true;
+
     if(this.isLoginMode){
 
     }else{
 
       this.authService.signUp(email, password).subscribe(responseData => {
         console.log(responseData)
-      }, error => {
-        console.log(error)
+        this.isLoading = false;
+      }, errorMessage => {
+        console.log(errorMessage)
+        this.error = errorMessage;
+        this.isLoading = false;
       })
 
     }
---
+++ src/app/shared/loading-spinner/loading-spinner.component.ts
@@ -0,0 +1,12 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: 'app-loading-spinner',
+  template: '<div class="loadingio-spinner-eclipse-11ze50anuq3"><div class="ldio-xnf6uykwy2c">' +
+    '<div></div>' +
+    '</div></div>',
+  styleUrls: ['./loading-spinner.css']
+})
+export class LoadingSpinnerComponent{
+
+}
