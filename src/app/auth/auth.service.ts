8a557096349c65e7ec76b730c742f19d8644842c
samuel <samuel.bruno.aguiar@gmail.com>
2023-06-18 15:12:39 -0300

    creating and storing user data

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,7 +1,8 @@
 import {Injectable} from "@angular/core";
 import {HttpClient, HttpErrorResponse} from "@angular/common/http";
-import {catchError} from "rxjs/operators";
-import {throwError} from "rxjs";
+import {catchError, tap} from "rxjs/operators";
+import {Subject, throwError} from "rxjs";
+import {User} from "./user.model";
 
 export interface AuthResponseData {
   idToken: string;
@@ -14,6 +15,7 @@ export interface AuthResponseData {
 
 @Injectable({providedIn: 'root'})
 export class AuthService {
+  user = new Subject<User>();
 
   constructor(private httpClient: HttpClient) {
   }
@@ -26,7 +28,16 @@ export class AuthService {
         password: password,
         returnSecureToken: true
       }
-    ).pipe(catchError(this.handleError));
+    ).pipe(
+      catchError(this.handleError),
+      tap(respData => {
+        this.handleAuthentication(
+          respData.email,
+          respData.localId,
+          respData.idToken,
+          +respData.expiresIn);
+      })
+    );
   }
 
   login(email: string, password: string) {
@@ -36,15 +47,34 @@ export class AuthService {
         email: email,
         password: password,
         returnSecureToken: true
-      }).pipe(catchError(this.handleError));
+      }).pipe(
+      catchError(this.handleError),
+      tap(respData => {
+        this.handleAuthentication(
+          respData.email,
+          respData.localId,
+          respData.idToken,
+          +respData.expiresIn);
+      })
+    );
+  }
+
+  private handleAuthentication(email: string, userId: string, token: string, expiresIn: number) {
+    const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
+    const user = new User(
+      email,
+      userId,
+      token,
+      expirationDate);
+    this.user.next(user);
   }
 
   private handleError(errorResponse: HttpErrorResponse) {
-    let errorMessage:string = 'An unknown errror occurred!';
-    if(!errorResponse.error || !errorResponse.error.error){
+    let errorMessage: string = 'An unknown errror occurred!';
+    if (!errorResponse.error || !errorResponse.error.error) {
       return throwError(errorMessage)
     }
-    switch (errorResponse.error.error.message){
+    switch (errorResponse.error.error.message) {
       case 'EMAIL_EXISTS':
         errorMessage = 'This email already exists';
         break;
