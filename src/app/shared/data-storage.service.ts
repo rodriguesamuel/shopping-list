b899646c1c079b153c0449ed99e7b11473b3c406
Samuel <samuel.bruno.aguiar@gmail.com>
2023-08-01 22:41:35 -0300

    attaching the token with an interceptor

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -20,19 +20,16 @@ export class DataStorageService {
   }
 
   fetchData() {
-    return this.authService.user.pipe(take(1), exhaustMap(user => {
-        return this.http.get<Recipe[]>('https://ng-course-recepie-book-3e6d6-default-rtdb.firebaseio.com/recipes.json',
-          {
-            params: new HttpParams().set('auth', user.token)
-          });
 
-      }), map(recipes => {
-        return recipes.map(recipe => {
-          return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
-        });
-      }),
-      tap(recipes => {
-        this.recipeService.setRecipes(recipes)
-      }));
+    return this.http.get<Recipe[]>('https://ng-course-recepie-book-3e6d6-default-rtdb.firebaseio.com/recipes.json')
+      .pipe(
+        map(recipes => {
+          return recipes.map(recipe => {
+            return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
+          });
+        }),
+        tap(recipes => {
+          this.recipeService.setRecipes(recipes)
+        }));
   }
 }
