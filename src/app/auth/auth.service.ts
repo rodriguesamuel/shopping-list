d3e6d1eb8da3f90e222486232f44eeca81e4c763
Samuel <samuel.bruno.aguiar@gmail.com>
2023-08-02 21:30:16 -0300

    adding auto logout

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -17,6 +17,8 @@ export interface AuthResponseData {
 @Injectable({providedIn: 'root'})
 export class AuthService {
   user = new BehaviorSubject<User>(null);
+  private tokenExpirationTimer: any;
+
 
   constructor(private httpClient: HttpClient, private router: Router) {
   }
@@ -60,9 +62,48 @@ export class AuthService {
     );
   }
 
-  logout(){
+  logout() {
     this.user.next(null);
     this.router.navigate(['/auth']);
+    localStorage.removeItem('userData');
+    if (this.tokenExpirationTimer) {
+      clearTimeout(this.tokenExpirationTimer);
+    }
+    this.tokenExpirationTimer = null;
+  }
+
+  autoLogout(expirationDuration: number) {
+    console.log(expirationDuration);
+    this.tokenExpirationTimer = setTimeout(() => {
+      this.logout();
+    }, expirationDuration);
+  }
+
+  autoLogin() {
+
+    const userData: {
+      email: string,
+      id: string,
+      _token: string,
+      _tokenExpirationDate: string
+    } = JSON.parse(localStorage.getItem('userData'));
+
+    if (!userData) {
+      return;
+    }
+
+    const loadedUser = new User(
+      userData.email,
+      userData.id,
+      userData._token,
+      new Date(userData._tokenExpirationDate)
+    );
+
+    if (loadedUser.token) {
+      this.user.next(loadedUser);
+      const expirationDuration = new Date(userData._tokenExpirationDate).getTime() - new Date().getTime();
+      this.autoLogout(expirationDuration);
+    }
   }
 
   private handleAuthentication(email: string, userId: string, token: string, expiresIn: number) {
@@ -73,7 +114,10 @@ export class AuthService {
       token,
       expirationDate);
     this.user.next(user);
+    this.autoLogout(expiresIn * 1000);
+    localStorage.setItem('userData', JSON.stringify(user));
   }
+
 
   private handleError(errorResponse: HttpErrorResponse) {
     let errorMessage: string = 'An unknown errror occurred!';
